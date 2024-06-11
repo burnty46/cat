6ee42ca26a1f759439c683ce985b81e4f4d31ad9
BMC <165616467+burnty46@users.noreply.github.com>
2024-06-10 20:54:44 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomGrabbableMod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.bmc.gorillatag.cat";
+        public const string Name = "cat";
+        public const string Version = "1.0.0";
+    }
+}
